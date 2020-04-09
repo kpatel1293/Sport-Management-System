b3c485aaa2aa28ec6cf0599bfbe1d6716546ddbf
Krishna Patel <kpatel1293@gmail.com>
2020-04-08 21:35:18 -0500

    initial commit

---
+++ src/main/java/edu/depaul/cdm/se/sportmanagementsystem/SportManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se.sportmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportManagementSystemApplication.class, args);
+	}
+
+}
