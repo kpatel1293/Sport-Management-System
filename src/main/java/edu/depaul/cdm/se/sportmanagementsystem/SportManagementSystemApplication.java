c45814fba3de93a1d40973b84ea1d0080f112bb4
Alejandro Amaya <amayaalex19@gmail.com>
2020-04-11 12:37:35 -0500

    test comment

--- src/main/java/edu/depaul/cdm/se/sportmanagementsystem/SportManagementSystemApplication.java
+++ src/main/java/edu/depaul/cdm/se/sportmanagementsystem/SportManagementSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportManagementSystemApplication {
+	//test comment
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportManagementSystemApplication.class, args);
