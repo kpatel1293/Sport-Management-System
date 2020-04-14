14862ee72869554bda10564d33acb3d75d075c6a
chiqui <imorhir@mail.depaul.edu>
2020-04-13 23:20:37 -0500

    last commit trial

--- src/main/java/edu/depaul/cdm/se/sportmanagementsystem/SportManagementSystemApplication.java
+++ src/main/java/edu/depaul/cdm/se/sportmanagementsystem/SportManagementSystemApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SportManagementSystemApplication {
 	//test comment
-	// issam coment to test my comit 
-	//second commit test 
+	 
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportManagementSystemApplication.class, args);
