26c2e4db5c00032c2ff187cfb09bc7f26765216e
Alejandro Amaya <amayaalex19@gmail.com>
2020-04-28 20:06:34 -0500

    added NOSQL playerGameRatings and playerSeasonRatings

---
+++ src/main/java/playerRatings/PlayerSeasonRatingsRepository.java
@@ -0,0 +1,7 @@
+package playerRatings;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlayerSeasonRatingsRepository extends CrudRepository<PlayerSeasonRatings, String>{
+
+}
