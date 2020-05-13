d31dc75a40b461a7aa283523db2286caa7a1cffc
Krishna Patel <kpatel1293@gmail.com>
2020-05-12 20:12:00 -0500

    flyway config in progress

--- src/main/java/edu/depaul/cdm/se/sportmanagementsystem/SportManagementSystemApplication.java
+++ src/main/java/edu/depaul/cdm/se/sportmanagementsystem/SportManagementSystemApplication.java
@@ -1,11 +1,17 @@
 package edu.depaul.cdm.se.sportmanagementsystem;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportManagementSystemApplication {
 	public static void main(String[] args) {
+		// Create the Flyway instance and point it to the database
+        Flyway flyway = Flyway.configure().dataSource("jdbc:h2:mem:SportManagement", "sa", null).load();
+		flyway.clean();
+        // Start the migration
+        flyway.migrate();
 		SpringApplication.run(SportManagementSystemApplication.class, args);
 	}
 }
