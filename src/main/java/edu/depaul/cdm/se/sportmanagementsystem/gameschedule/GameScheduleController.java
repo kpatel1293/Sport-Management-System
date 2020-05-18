96e69f76ccfa373bc87d793bd8969bd874db811b
Krishna Patel <kpatel1293@gmail.com>
2020-05-17 21:33:05 -0500

    added issam's work

--- src/main/java/edu/depaul/cdm/se/sportmanagementsystem/gameschedule/GameScheduleController.java
+++ src/main/java/edu/depaul/cdm/se/sportmanagementsystem/gameschedule/GameScheduleController.java
@@ -1,6 +1,6 @@
 package edu.depaul.cdm.se.sportmanagementsystem.gameschedule;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,27 +9,39 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@RestController
+@Controller
 @RequestMapping("/api/v1")
 public class GameScheduleController {
     @Autowired
     GameScheduleService gameScheduleService;
 
     // get all game schedules
-    @GetMapping("/games")
-    public List<GameSchedule> getAllGames() {
-        return gameScheduleService.getAllGames();
-    }
+//    @GetMapping("/games")
+//    public List<GameSchedule> getAllGames() {
+//        return gameScheduleService.getAllGames();
+//    }
+    
+    
+    @GetMapping("/all")
+ 	public String getAllGames(Model model) {
+     	model.addAttribute("game_schedule", gameScheduleService.getAllGames());
+ 			return "schedule-list";
+ 	}
 
     // get a game schedule
     @GetMapping("/games/{id}")
@@ -58,11 +70,35 @@ public class GameScheduleController {
     }
 
     // save a game schedule
-    @PostMapping(value="/games")
-    public ResponseEntity<GameSchedule> createSchedule(@Valid @RequestBody GameSchedule gameSchedule) {
-        gameScheduleService.saveGameSchedule(gameSchedule);
-        
-        return ResponseEntity.ok().body(gameSchedule);
+//    @PostMapping(value="/games")
+//    public ResponseEntity<GameSchedule> createSchedule(@Valid @RequestBody GameSchedule gameSchedule) {
+//        gameScheduleService.saveGameSchedule(gameSchedule);
+//        
+//        return ResponseEntity.ok().body(gameSchedule);
+//    }
+    
+    @GetMapping(value = "/redirect-add")
+    public String redirectAddSchedule( Model model) {
+    	model.addAttribute("schedule", new GameSchedule());
+    	return "schedule-input";
+    }
+    
+//    @PostMapping(value = "/save-schedule")
+//    public String createSchedule(@ModelAttribute  GameSchedule schedule, Model model) {
+// 		//gameScheduleService.saveGameSchedule(schedule);
+//    	
+//      return "schedule-list";
+//    }
+    //@PostMapping("/save-schedule")
+   @RequestMapping(value="/save-schedule", method=RequestMethod.POST)
+    public String createSchedule(@Valid @ModelAttribute("schedule") GameSchedule schedule, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "schedule-input";
+        }
+         
+        gameScheduleService.saveGameSchedule(schedule);
+        model.addAttribute("game_schedule", gameScheduleService.getAllGames());
+        return "schedule-list";
     }
     
     
